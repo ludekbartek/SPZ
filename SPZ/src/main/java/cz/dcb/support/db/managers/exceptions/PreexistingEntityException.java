9f518a3add2c6081504873e005edb65da8376862
ludekbartek <ludek.bartek@seznam.cz>
2015-06-28 06:06:46 +0200

    Vyjimky vyuzivane JPA implementacemi spravcu entit.

---
+++ SPZ/src/main/java/cz/dcb/support/db/managers/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package cz.dcb.support.db.managers.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
