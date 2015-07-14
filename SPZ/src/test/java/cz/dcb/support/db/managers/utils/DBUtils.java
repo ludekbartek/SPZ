2ae88ec2f9945c6d4cc4c46093961e62850b3c08
ludekbartek <ludek.bartek@seznam.cz>
2015-07-14 06:17:21 +0200

    Pomocna trida pro praci s DB.

---
+++ SPZ/src/test/java/cz/dcb/support/db/managers/utils/DBUtils.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.dcb.support.db.managers.utils;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author bar
+ */
+public class DBUtils {
+    public static EntityManagerFactory getEntityManagerFactory()
+    {
+        return Persistence.createEntityManagerFactory("support_JPA");
+    }
+}
