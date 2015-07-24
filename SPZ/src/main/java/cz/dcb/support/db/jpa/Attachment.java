f7202ef59fcd0f30b062a6a4600f6b17db3f710e
ludekbartek <ludek.bartek@seznam.cz>
2015-07-24 06:37:48 +0200

    Pridana anotace pro automaticke generovani primarniho klice.

--- SPZ/src/main/java/cz/dcb/support/db/jpa/Attachment.java
+++ SPZ/src/main/java/cz/dcb/support/db/jpa/Attachment.java
@@ -9,6 +9,8 @@ import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -28,6 +30,7 @@ public class Attachment implements Serializable {
     @Basic
     private String location;
     @Column(name="id",table="attachment",nullable=false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
     @Id
     private Integer id;
     @ManyToOne(optional=false,targetEntity = Spznote.class)
