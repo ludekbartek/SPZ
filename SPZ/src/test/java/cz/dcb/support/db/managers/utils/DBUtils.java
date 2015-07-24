7ce80e988bab5e6971258fea5a83d7cbdd6b807f
ludekbartek <ludek.bartek@seznam.cz>
2015-07-24 13:37:53 +0200

    Pridany metody pro vytvoreni entit (nutno odladit, zda maji vse).

--- SPZ/src/test/java/cz/dcb/support/db/managers/utils/DBUtils.java
+++ SPZ/src/test/java/cz/dcb/support/db/managers/utils/DBUtils.java
@@ -5,6 +5,12 @@
  */
 package cz.dcb.support.db.managers.utils;
 
+import cz.dcb.support.db.jpa.Attachment;
+import cz.dcb.support.db.jpa.Spznote;
+import cz.dcb.support.db.jpa.Spzstate;
+import cz.dcb.support.db.jpa.User;
+import java.util.GregorianCalendar;
+import java.util.Random;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -16,5 +22,49 @@ public class DBUtils {
     public static EntityManagerFactory getEntityManagerFactory()
     {
         return Persistence.createEntityManagerFactory("support_JPA");
+    }
+    
+    public static Attachment createAttachment(){
+        Attachment atach = new Attachment();
+        atach.setContent("Some content");
+        atach.setDate(new GregorianCalendar().getTime());
+        atach.setLocation("/some/path/");
+        atach.setSpznoteId(createSpznote());
+        atach.setType("text/plain");
+        atach.setTs(new Random().nextInt()%1000);
+        return atach;
+    }
+    
+    public static Spznote createSpznote(){
+        Spznote note = new Spznote();
+        note.setExternalNote((short)0);
+        note.setIssuerLogin(createUser());
+        note.setNoteDate(new GregorianCalendar().getTime());
+        note.setNtext("Text poznamky");
+        note.setStateId(createSpzState());
+        note.setTs(new Random().nextInt()%1000);
+        return note;
+    }
+    
+    public static Spzstate createSpzState(){
+        Spzstate state = new Spzstate();
+        state.setAssumedManDays(1.5);
+        state.setCode("Started");
+        state.setIssuerLogin(createUser());
+        
+        return state;
+    }
+    
+    public static User createUser(){
+        User user = new User();
+        user.setCompany("aaa");
+        user.setEmail("someb@aaa.cz");
+        user.setFax("123");
+        user.setLogin("aaa");
+        user.setName("A Aa");
+        user.setPassword("bbb");
+        user.setTel("213456");
+        user.setTs(1);
+        return user;
     }
 }
