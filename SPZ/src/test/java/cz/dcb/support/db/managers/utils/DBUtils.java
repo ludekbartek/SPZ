5eaad2973ac5bb750353ad2597101eb57c482bde
ludekbartek <ludek.bartek@seznam.cz>
2015-07-25 12:15:33 +0200

    Upravy metod pro vytvareni testovacich dat, aby nedochazelo k porusovani integritnich omezeni.

--- SPZ/src/test/java/cz/dcb/support/db/managers/utils/DBUtils.java
+++ SPZ/src/test/java/cz/dcb/support/db/managers/utils/DBUtils.java
@@ -10,7 +10,9 @@ import cz.dcb.support.db.jpa.Spznote;
 import cz.dcb.support.db.jpa.Spzstate;
 import cz.dcb.support.db.jpa.User;
 import java.util.GregorianCalendar;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -29,19 +31,23 @@ public class DBUtils {
         atach.setContent("Some content");
         atach.setDate(new GregorianCalendar().getTime());
         atach.setLocation("/some/path/");
-        atach.setSpznoteId(createSpznote());
+        Spzstate state = createSpzState();
+        atach.setSpznoteId(createSpznote(state));
         atach.setType("text/plain");
-        atach.setTs(new Random().nextInt()%1000);
+        atach.setTs(1000);
         return atach;
     }
     
-    public static Spznote createSpznote(){
+    public static Spznote createSpznote(Spzstate state){
         Spznote note = new Spznote();
         note.setExternalNote((short)0);
         note.setIssuerLogin(createUser());
         note.setNoteDate(new GregorianCalendar().getTime());
         note.setNtext("Text poznamky");
-        note.setStateId(createSpzState());
+        note.setStateId(state);
+        Set<Spznote> notes = new HashSet<>();
+        notes.add(note);
+        state.setSpznoteCollection(notes);
         note.setTs(new Random().nextInt()%1000);
         return note;
     }
@@ -51,6 +57,7 @@ public class DBUtils {
         state.setAssumedManDays(1.5);
         state.setCode("Started");
         state.setIssuerLogin(createUser());
+        
         
         return state;
     }
