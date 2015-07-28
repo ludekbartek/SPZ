5462ece280a0fd6dc9a42577dd8e5f019c2b25d1
ludekbartek <ludek.bartek@seznam.cz>
2015-07-28 08:40:19 +0200

    Pridany testy

--- SPZ/src/test/java/cz/dcb/support/db/managers/SpzManagerTest.java
+++ SPZ/src/test/java/cz/dcb/support/db/managers/SpzManagerTest.java
@@ -5,11 +5,18 @@
  */
 package cz.dcb.support.db.managers;
 
+import cz.dcb.support.db.jpa.Configuration;
+import cz.dcb.support.db.jpa.Project;
 import cz.dcb.support.db.jpa.Spz;
+import cz.dcb.support.db.jpa.User;
 import cz.dcb.support.db.managers.exceptions.IllegalOrphanException;
 import cz.dcb.support.db.managers.exceptions.NonexistentEntityException;
 import cz.dcb.support.db.managers.exceptions.PreexistingEntityException;
+import cz.dcb.support.db.managers.utils.DBUtils;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManagerFactory;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -23,7 +30,15 @@ import static org.junit.Assert.*;
  */
 public class SpzManagerTest {
     
+    private SpzManager manager;
+    private UserManager userManager;
+    
+    private static final Logger LOGGER = Logger.getLogger(SpzManagerTest.class.getName());
+    
     public SpzManagerTest() {
+        EntityManagerFactory emf = DBUtils.getEntityManagerFactory();
+        manager = new SpzJpaController(emf);
+        userManager = new UserJpaController(emf);
     }
     
     @BeforeClass
@@ -40,6 +55,11 @@ public class SpzManagerTest {
     
     @After
     public void tearDown() {
+        EntityManagerFactory emf = DBUtils.getEntityManagerFactory();
+        deleteSpzs(emf);
+        deleteConfigurations(emf);
+        deleteProjects(emf);
+        deleteUsers(emf);
     }
 
     /**
@@ -47,12 +67,11 @@ public class SpzManagerTest {
      */
     @Test
     public void testCreate() throws Exception {
-        System.out.println("create");
-        Spz spz = null;
-        SpzManager instance = new SpzManagerImpl();
-        instance.create(spz);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("create correct Spz");
+        Spz spz = DBUtils.createSpz();
+        manager.create(spz);
+        
+        
     }
 
     /**
@@ -138,6 +157,50 @@ public class SpzManagerTest {
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
+    }
+
+    private void deleteSpzs(EntityManagerFactory emf) {
+        SpzManager manager = new SpzJpaController(emf);
+        for(Spz spz:manager.findSpzEntities()){
+            try {
+                manager.destroy(spz.getId());
+            } catch (IllegalOrphanException | NonexistentEntityException ex) {
+                Logger.getLogger(SpzManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private void deleteConfigurations(EntityManagerFactory emf) {
+        ConfigurationManager manager = new ConfigurationJpaController(emf);
+        for(Configuration conf:manager.findConfigurationEntities()){
+            try {
+                manager.destroy(conf.getId());
+            } catch (IllegalOrphanException | NonexistentEntityException ex) {
+                Logger.getLogger(SpzManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private void deleteProjects(EntityManagerFactory emf) {
+        ProjectManager manager = new ProjectJpaController(emf);
+        for(Project proj:manager.findProjectEntities()){
+            try {
+                manager.destroy(proj.getName());
+            } catch (IllegalOrphanException | NonexistentEntityException ex) {
+                Logger.getLogger(SpzManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private void deleteUsers(EntityManagerFactory emf) {
+        UserManager manager = new UserJpaController(emf);
+        for(User user:manager.findUserEntities()){
+            try {
+                manager.destroy(user.getLogin());
+            } catch (IllegalOrphanException | NonexistentEntityException ex) {
+                Logger.getLogger(SpzManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     public class SpzManagerImpl implements SpzManager {
