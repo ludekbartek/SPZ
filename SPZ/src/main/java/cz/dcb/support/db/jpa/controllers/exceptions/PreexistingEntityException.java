038f62dc287a60a4ae82e54dc96db83421c7ec3c
ludekbartek <ludek.bartek@seznam.cz>
2015-07-28 09:14:37 +0200

    JPA Exceptions

---
+++ SPZ/src/main/java/cz/dcb/support/db/jpa/controllers/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package cz.dcb.support.db.jpa.controllers.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
