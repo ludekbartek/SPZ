2ecdbb5ea97bef76ae4f4c2882a5a1814a595035
ludekbartek <ludek.bartek@seznam.cz>
2015-07-28 08:40:41 +0200

    Pridany pomocne metody pro vytvareni entit.

--- SPZ/src/test/java/cz/dcb/support/db/managers/utils/DBUtils.java
+++ SPZ/src/test/java/cz/dcb/support/db/managers/utils/DBUtils.java
@@ -6,9 +6,18 @@
 package cz.dcb.support.db.managers.utils;
 
 import cz.dcb.support.db.jpa.Attachment;
+import cz.dcb.support.db.jpa.Configuration;
+import cz.dcb.support.db.jpa.Project;
+import cz.dcb.support.db.jpa.Spz;
 import cz.dcb.support.db.jpa.Spznote;
 import cz.dcb.support.db.jpa.Spzstate;
 import cz.dcb.support.db.jpa.User;
+import cz.dcb.support.db.managers.ConfigurationJpaController;
+import cz.dcb.support.db.managers.ConfigurationManager;
+import cz.dcb.support.db.managers.ProjectJpaController;
+import cz.dcb.support.db.managers.ProjectManager;
+import cz.dcb.support.db.managers.UserJpaController;
+import cz.dcb.support.db.managers.UserManager;
 import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.Random;
@@ -58,7 +67,6 @@ public class DBUtils {
         state.setCode("Started");
         state.setIssuerLogin(createUser());
         
-        
         return state;
     }
     
@@ -73,5 +81,54 @@ public class DBUtils {
         user.setTel("213456");
         user.setTs(1);
         return user;
+    }
+    
+    public static Spz createSpz() throws Exception {
+        EntityManagerFactory emf = getEntityManagerFactory();
+        User user = createUser();
+        UserManager userManager = new UserJpaController(emf);
+        userManager.create(user);
+        ConfigurationManager confManager = new ConfigurationJpaController(emf);
+        Configuration config = DBUtils.createConfiguration();
+        confManager.create(config);
+        
+        GregorianCalendar cal = new GregorianCalendar(2015, 7, 29);
+        Spz spz = new Spz();
+        spz.setAnalystLogin(user);
+        spz.setDeveloperLogin(user);
+        spz.setContactPerson(user.getName());
+        spz.setImplementationAcceptDate(cal.getTime());
+        spz.setIssueDate(cal.getTime());
+        spz.setIssuerLogin(user.getLogin());
+        spz.setReqNumber("1");
+        spz.setRequestDescription("Some description");
+        spz.setRequestType("radna");
+        spz.setShortName("test");
+        spz.setConfigurationId(config);
+        return spz;
+    }
+
+    private static Configuration createConfiguration() throws Exception {
+        EntityManagerFactory emf = DBUtils.getEntityManagerFactory();
+        ProjectManager projMan = new ProjectJpaController(emf);
+        Project project = DBUtils.createProject();
+        projMan.create(project);
+        
+        Configuration config = new Configuration();
+        
+        config.setCode("Some code");
+        config.setDescription("Some description");
+        config.setProjectCode(project);
+        config.setSeqnumber(1);
+        
+        return config;
+    }
+
+    private static Project createProject() {
+        Project project = new Project();
+        project.setDescription("Some project description");
+        project.setName("Test project");
+        project.setTs(1);
+        return project;
     }
 }
