bbee13c8cf3e03f35405a8205ed0fcb10aefd73d
ludekbartek <ludek.bartek@seznam.cz>
2015-07-28 09:55:02 +0200

    Modification according to db change.

--- SPZ/src/test/java/cz/dcb/support/db/managers/utils/DBUtils.java
+++ SPZ/src/test/java/cz/dcb/support/db/managers/utils/DBUtils.java
@@ -5,10 +5,11 @@
  */
 package cz.dcb.support.db.managers.utils;
 
-import cz.dcb.support.db.jpa.Attachment;
-import cz.dcb.support.db.jpa.Spznote;
-import cz.dcb.support.db.jpa.Spzstate;
-import cz.dcb.support.db.jpa.User;
+import cz.dcb.support.db.jpa.entities.Attachment;
+import cz.dcb.support.db.jpa.entities.Spznote;
+import cz.dcb.support.db.jpa.entities.Spzstate;
+import cz.dcb.support.db.jpa.entities.User;
+import java.math.BigInteger;
 import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.Random;
@@ -32,31 +33,30 @@ public class DBUtils {
         atach.setDate(new GregorianCalendar().getTime());
         atach.setLocation("/some/path/");
         Spzstate state = createSpzState();
-        atach.setSpznoteId(createSpznote(state));
+        //atach.setSpznoteId(createSpznote(state));
         atach.setType("text/plain");
-        atach.setTs(1000);
+        atach.setTs(BigInteger.valueOf(1000));
         return atach;
     }
     
     public static Spznote createSpznote(Spzstate state){
         Spznote note = new Spznote();
-        note.setExternalNote((short)0);
-        note.setIssuerLogin(createUser());
-        note.setNoteDate(new GregorianCalendar().getTime());
-        note.setNtext("Text poznamky");
-        note.setStateId(state);
+        note.setExternalnote((short)1);
+        note.setNotedate(new GregorianCalendar().getTime());
+        note.setNotetext("Text poznamky");
+        //note.setStateId(state);
         Set<Spznote> notes = new HashSet<>();
         notes.add(note);
-        state.setSpznoteCollection(notes);
-        note.setTs(new Random().nextInt()%1000);
+        //state.setSpznoteCollection(notes);
+        note.setTs(BigInteger.valueOf(new Random().nextInt()%1000));
         return note;
     }
     
     public static Spzstate createSpzState(){
         Spzstate state = new Spzstate();
-        state.setAssumedManDays(1.5);
+        state.setAssumedmandays(1.5);
         state.setCode("Started");
-        state.setIssuerLogin(createUser());
+        state.setIssuerLogin(createUser().getLogin());
         
         
         return state;
@@ -70,8 +70,8 @@ public class DBUtils {
         user.setLogin("aaa");
         user.setName("A Aa");
         user.setPassword("bbb");
-        user.setTel("213456");
-        user.setTs(1);
+        user.setTel("123123");
+        
         return user;
     }
 }
