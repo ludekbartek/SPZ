c153af1369aa434b935552afb7c9f2153db1208a
ludekbartek <ludek.bartek@seznam.cz>
2015-08-15 19:34:45 +0200

    Test of SpzIssuerManager method create.

--- SPZ/src/test/java/cz/dcb/support/db/managers/SpzIssuerManagerTest.java
+++ SPZ/src/test/java/cz/dcb/support/db/managers/SpzIssuerManagerTest.java
@@ -5,11 +5,23 @@
  */
 package cz.dcb.support.db.managers;
 
+import cz.dcb.support.db.jpa.controllers.SpzIssuerJpaController;
 import cz.dcb.support.db.jpa.controllers.SpzIssuerManager;
+import cz.dcb.support.db.jpa.controllers.SpzJpaController;
+import cz.dcb.support.db.jpa.controllers.SpzManager;
+import cz.dcb.support.db.jpa.controllers.UserJpaController;
+import cz.dcb.support.db.jpa.controllers.UserManager;
 import cz.dcb.support.db.jpa.controllers.exceptions.NonexistentEntityException;
+import cz.dcb.support.db.jpa.entities.Spz;
 import cz.dcb.support.db.jpa.entities.Spzissuer;
+import cz.dcb.support.db.jpa.entities.User;
+import cz.dcb.support.db.managers.utils.DBUtils;
+
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -22,6 +34,9 @@ import static org.junit.Assert.*;
  * @author bar
  */
 public class SpzIssuerManagerTest {
+
+    private final SpzIssuerManager manager = new SpzIssuerJpaController(DBUtils.getEntityManagerFactory());
+    private static final Logger logger = Logger.getLogger(SpzIssuerManagerTest.class.getName());
     
     public SpzIssuerManagerTest() {
     }
@@ -36,10 +51,12 @@ public class SpzIssuerManagerTest {
     
     @Before
     public void setUp() {
+        clearDB();
     }
     
     @After
     public void tearDown() {
+        clearDB();
     }
 
     /**
@@ -48,24 +65,25 @@ public class SpzIssuerManagerTest {
     @Test
     public void testCreate() {
         System.out.println("create");
-        Spzissuer spzissuer = null;
-        SpzIssuerManager instance = new SpzIssuerManagerImpl();
-        instance.create(spzissuer);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of destroy method, of class SpzIssuerManager.
-     */
-    @Test
-    public void testDestroy() throws Exception {
-        System.out.println("destroy");
-        Integer id = null;
-        SpzIssuerManager instance = new SpzIssuerManagerImpl();
-        instance.destroy(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Spzissuer spzissuer = null, result = null;
+        //SpzIssuerManager instance = new SpzIssuerManagerImpl();
+        try{
+            manager.create(spzissuer);
+            fail("Created null spzissuer record.");
+        }catch(IllegalArgumentException iae){
+            logger.log(Level.INFO,"Exception ", iae);
+        }catch(Exception ex){
+            fail("Invalid exception thrown.");
+        }
+        
+        spzissuer = DBUtils.createSpzIssuer();
+        try{
+            manager.create(spzissuer);
+        }catch(Exception ex){
+            fail("Unexpected exception "+ex);
+        }
+        result = manager.findSpzissuer(spzissuer.getId());
+        assertEquals(spzissuer, result);
     }
 
     /**
@@ -152,6 +170,35 @@ public class SpzIssuerManagerTest {
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
+    }
+
+    private void clearDB() {
+        EntityManagerFactory emf = DBUtils.getEntityManagerFactory();
+        SpzManager spzManager = new SpzJpaController(emf);
+        UserManager userManager = new UserJpaController(emf);
+        
+        for(Spzissuer issuer:manager.findSpzissuerEntities()){
+            try {
+                manager.destroy(issuer.getId());
+            } catch (NonexistentEntityException ex) {
+                Logger.getLogger(SpzIssuerManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        for(Spz spz:spzManager.findSpzEntities()){
+            try {
+                spzManager.destroy(spz.getId());
+            } catch (NonexistentEntityException ex) {
+                Logger.getLogger(SpzIssuerManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        for(User user:userManager.findUserEntities()){
+            try {
+                userManager.destroy(user.getId());
+            } catch (NonexistentEntityException ex) {
+                Logger.getLogger(SpzIssuerManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
     }
 
     public class SpzIssuerManagerImpl implements SpzIssuerManager {
