2b2582d8bc92517dbd1ed52fdeb2663973bbebf3
ludekbartek <ludek.bartek@seznam.cz>
2015-08-16 19:31:39 +0200

    Tests for methods create and correct edits.

--- SPZ/src/test/java/cz/dcb/support/db/managers/SpzManagerTest.java
+++ SPZ/src/test/java/cz/dcb/support/db/managers/SpzManagerTest.java
@@ -12,7 +12,14 @@ import cz.dcb.support.db.managers.exceptions.IllegalOrphanException;
 import cz.dcb.support.db.managers.exceptions.NonexistentEntityException;
 import cz.dcb.support.db.managers.exceptions.PreexistingEntityException;
 import cz.dcb.support.db.managers.utils.DBUtils;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -25,25 +32,32 @@ import static org.junit.Assert.*;
  * @author bar
  */
 public class SpzManagerTest {
+    
     private final SpzManager manager = new SpzJpaController(DBUtils.getEntityManagerFactory());
-   
+    private static final Logger LOGGER=Logger.getLogger(SpzManagerTest.class.getName());
+    private static final int MAX_SPZS = 95;
+    
     public SpzManagerTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+        
     }
     
     @AfterClass
     public static void tearDownClass() {
+        
     }
     
     @Before
     public void setUp() {
+        clearDB();
     }
     
     @After
     public void tearDown() {
+        clearDB();
     }
 
     /**
@@ -54,24 +68,21 @@ public class SpzManagerTest {
         System.out.println("create");
         Spz spz = null;
 //        SpzManager instance = new SpzManagerImpl();
+        try{
+            manager.create(spz);
+            fail("Exception should be thrown.");
+        }catch(IllegalArgumentException iae){
+            LOGGER.log(Level.INFO,"Exception ",iae);
+        }catch(Exception ex){
+            fail("Invalid exception thrown " + ex);
+        }
+        spz = DBUtils.createSpz();
         manager.create(spz);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNotNull(spz.getId());
+        Spz result = manager.findSpz(spz.getId());
     }
 
-    /**
-     * Test of destroy method, of class SpzManager.
-     */
-    @Test
-    public void testDestroy() throws Exception {
-        System.out.println("destroy");
-        Integer id = null;
-        //SpzManager instance = new SpzManagerImpl();
-        manager.destroy(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
+    
     /**
      * Test of edit method, of class SpzManager.
      */
@@ -80,9 +91,65 @@ public class SpzManagerTest {
         System.out.println("edit");
         Spz spz = null;
         //SpzManager instance = new SpzManagerImpl();
+        try{
+            manager.edit(spz);
+            fail("Exception should be thrown.");
+        }catch(IllegalArgumentException iae){
+            LOGGER.log(Level.INFO,"Exception ",iae);
+        }catch(Exception ex){
+            fail("Unexpected exception trhown "+ex);
+        }
+        
+        Random rand = new Random();
+        int count = rand.nextInt(MAX_SPZS)+5;
+        List<Spz> spzs = createSpzs(count);
+        int idx = rand.nextInt(count);
+        spz = spzs.get(idx);
+        
+        spz.setContactperson("Novy uzivatel 1");
+         
         manager.edit(spz);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        Spz result = manager.findSpz(spz.getId());
+        assertEquals(result, spz);
+        
+        Calendar cal = new GregorianCalendar();
+        
+        spz.setImplementationacceptdate(cal.getTime());
+        checkResult(spz);
+        
+        spz.setIssuedate(cal.getTime());
+        checkResult(spz);
+        
+        spz.setPriority(Short.MAX_VALUE);
+        checkResult(spz);
+        
+        spz.setPriority(Short.MIN_VALUE);
+        checkResult(spz);
+        
+        spz.setReqnumber("ZERO");
+        checkResult(spz);
+        
+        spz.setRequestdescription("Testing request");
+        checkResult(spz);
+        
+        spz.setRequesttype("Modifikace datovych struktur.");
+        checkResult(spz);
+        
+        spz.setShortname("Nove kratke jmeno.");
+        checkResult(spz);
+        
+        spz.setTs(new BigInteger("10000000000"));
+        checkResult(spz);
+    }
+
+    @Test
+    public void testInvalidEdits() throws Exception{
+        fail("Test case prototype");
+    }
+    private void checkResult(Spz spz) throws Exception {
+        manager.edit(spz);
+        assertEquals(manager.findSpz(spz.getId()), spz);
     }
 
     /**
@@ -171,5 +238,25 @@ public class SpzManagerTest {
             return 0;
         }
     }*/
+    
+    private void clearDB() {
+        for(Spz spz:manager.findSpzEntities()){
+            try {
+                manager.destroy(spz.getId());
+            } catch (cz.dcb.support.db.jpa.controllers.exceptions.NonexistentEntityException ex) {
+                Logger.getLogger(SpzManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private List<Spz> createSpzs(int count) {
+        List<Spz> values = new ArrayList<>();
+        for(int i=0;i<count;i++){
+            Spz spz = DBUtils.createSpz();
+            manager.create(spz);
+            values.add(spz);
+        }
+        return values;
+    }
     
 }
