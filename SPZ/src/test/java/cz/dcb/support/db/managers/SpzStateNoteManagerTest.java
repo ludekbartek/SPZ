309980db885cedf281dafdb58ef0766088f2a81f
ludekbartek <ludek.bartek@seznam.cz>
2015-08-19 17:32:28 +0200

    Create skeleton of edit and SpzStateNoteCount tests created.

--- SPZ/src/test/java/cz/dcb/support/db/managers/SpzStateNoteManagerTest.java
+++ SPZ/src/test/java/cz/dcb/support/db/managers/SpzStateNoteManagerTest.java
@@ -5,11 +5,30 @@
  */
 package cz.dcb.support.db.managers;
 
+import cz.dcb.support.db.jpa.controllers.SpzJpaController;
+import cz.dcb.support.db.jpa.controllers.SpzManager;
+import cz.dcb.support.db.jpa.controllers.SpzNoteJpaController;
+import cz.dcb.support.db.jpa.controllers.SpzNoteManager;
+import cz.dcb.support.db.jpa.controllers.SpzStateJpaController;
+import cz.dcb.support.db.jpa.controllers.SpzStateManager;
+import cz.dcb.support.db.jpa.controllers.SpzStateNoteJpaController;
 import cz.dcb.support.db.jpa.controllers.SpzStateNoteManager;
+import cz.dcb.support.db.jpa.controllers.UserJpaController;
+import cz.dcb.support.db.jpa.controllers.UserManager;
 import cz.dcb.support.db.jpa.controllers.exceptions.NonexistentEntityException;
+import cz.dcb.support.db.jpa.entities.Spz;
+import cz.dcb.support.db.jpa.entities.Spznote;
+import cz.dcb.support.db.jpa.entities.Spzstate;
 import cz.dcb.support.db.jpa.entities.Spzstatenote;
+import cz.dcb.support.db.jpa.entities.User;
+import cz.dcb.support.db.managers.utils.DBUtils;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -22,6 +41,10 @@ import static org.junit.Assert.*;
  * @author bar
  */
 public class SpzStateNoteManagerTest {
+    
+    private SpzStateNoteManager manager = new SpzStateNoteJpaController(DBUtils.getEntityManagerFactory());
+    private static final Logger LOGGER = Logger.getLogger(SpzStateNoteManagerTest.class.getName());
+    private static final int MAX_SPZ_STATE_NOTES=90;
     
     public SpzStateNoteManagerTest() {
     }
@@ -36,10 +59,12 @@ public class SpzStateNoteManagerTest {
     
     @Before
     public void setUp() {
+        clearDB();
     }
     
     @After
     public void tearDown() {
+        clearDB();
     }
 
     /**
@@ -49,25 +74,31 @@ public class SpzStateNoteManagerTest {
     public void testCreate() {
         System.out.println("create");
         Spzstatenote spzstatenote = null;
-        SpzStateNoteManager instance = new SpzStateNoteManagerImpl();
-        instance.create(spzstatenote);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        try{
+            manager.create(spzstatenote);
+            fail("Exception should be thrown.");
+        }catch(IllegalArgumentException iae){
+            LOGGER.log(Level.INFO,"Exception ", iae);
+        }catch(Exception ex){
+            fail("Unexpected exception thrown: "+ex);
+        }
+        
+        spzstatenote = DBUtils.createSpzStateNote();
+        try{
+            manager.create(spzstatenote);
+        }catch(Exception ex){
+            fail("Unexpected exception trhown:"+ex);
+        }
+        assertNotNull(spzstatenote.getId());
+        Spzstatenote result = manager.findSpzstatenote(spzstatenote.getId());
+        assertEquals(spzstatenote, result);
     }
 
     /**
      * Test of destroy method, of class SpzStateNoteManager.
      */
-    @Test
-    public void testDestroy() throws Exception {
-        System.out.println("destroy");
-        Integer id = null;
-        SpzStateNoteManager instance = new SpzStateNoteManagerImpl();
-        instance.destroy(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
+    
     /**
      * Test of edit method, of class SpzStateNoteManager.
      */
@@ -75,10 +106,17 @@ public class SpzStateNoteManagerTest {
     public void testEdit() throws Exception {
         System.out.println("edit");
         Spzstatenote spzstatenote = null;
-        SpzStateNoteManager instance = new SpzStateNoteManagerImpl();
-        instance.edit(spzstatenote);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        try{
+            manager.edit(spzstatenote);
+            fail("Exception should be thrown.");
+        }catch(IllegalArgumentException iae){
+            LOGGER.log(Level.INFO,"Exception: ", iae);
+        }catch(Exception ex){
+            fail("Unexpected exception thrown.");
+        }
+        Random rand = new Random();
+        int count = rand.nextInt(MAX_SPZ_STATE_NOTES);
+        List<Spzstatenote> spzStateNotes = createSpzStateNotes(count);
     }
 
     /**
@@ -146,12 +184,72 @@ public class SpzStateNoteManagerTest {
     @Test
     public void testGetSpzstatenoteCount() {
         System.out.println("getSpzstatenoteCount");
-        SpzStateNoteManager instance = new SpzStateNoteManagerImpl();
+        
         int expResult = 0;
-        int result = instance.getSpzstatenoteCount();
+        int result = manager.getSpzstatenoteCount();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        Random rand = new Random();
+        int count = rand.nextInt(MAX_SPZ_STATE_NOTES);
+        List<Spzstatenote> values=createSpzStateNotes(count);
+        expResult = values.size();
+        result = manager.getSpzstatenoteCount();
+        assertEquals(expResult, result);
+        
+    }
+
+    private List<Spzstatenote> createSpzStateNotes(int count) {
+        List<Spzstatenote> values = new ArrayList<>();
+        for(int i=0;i<count;i++){
+            Spzstatenote spzStateNote = DBUtils.createSpzStateNote();
+            manager.create(spzStateNote);
+            values.add(spzStateNote);
+        }
+        return values;
+    }
+
+    private void clearDB() {
+        EntityManagerFactory emf = DBUtils.getEntityManagerFactory();
+        SpzManager spzManager = new SpzJpaController(emf);
+        SpzStateManager spzStateManger = new SpzStateJpaController(emf);
+        SpzNoteManager spzNoteManager = new SpzNoteJpaController(emf);
+        UserManager userManager = new UserJpaController(emf);
+        
+        for(Spz spz:spzManager.findSpzEntities()){
+            try {
+                manager.destroy(spz.getId());
+            } catch (NonexistentEntityException ex) {
+                Logger.getLogger(SpzStateNoteManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        for(User user:userManager.findUserEntities()){
+            try {
+                userManager.destroy(user.getId());
+            } catch (NonexistentEntityException ex) {
+                Logger.getLogger(SpzStateNoteManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        for(Spzstate state:spzStateManger.findSpzstateEntities()){
+            try {
+                spzStateManger.destroy(state.getId());
+            } catch (NonexistentEntityException ex) {
+                Logger.getLogger(SpzStateNoteManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        for(Spznote note:spzNoteManager.findSpznoteEntities()){
+            try {
+                spzNoteManager.destroy(note.getId());
+            } catch (NonexistentEntityException ex) {
+                Logger.getLogger(SpzStateNoteManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        for(Spzstatenote stateNote:manager.findSpzstatenoteEntities()){
+            try {
+                manager.destroy(stateNote.getId());
+            } catch (NonexistentEntityException ex) {
+                Logger.getLogger(SpzStateNoteManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     public class SpzStateNoteManagerImpl implements SpzStateNoteManager {
