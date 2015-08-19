35558893dddfb6f0bd28489364256416b1f6d08e
ludekbartek <ludek.bartek@seznam.cz>
2015-08-19 20:56:39 +0200

    Tests for create and edit created.

--- SPZ/src/test/java/cz/dcb/support/db/managers/SpznoteManagerTest.java
+++ SPZ/src/test/java/cz/dcb/support/db/managers/SpznoteManagerTest.java
@@ -7,12 +7,22 @@ package cz.dcb.support.db.managers;
 
 import cz.dcb.support.db.jpa.controllers.SpzNoteJpaController;
 import cz.dcb.support.db.jpa.controllers.SpzNoteManager;
+import cz.dcb.support.db.jpa.controllers.SpzStateJpaController;
+import cz.dcb.support.db.jpa.controllers.SpzStateManager;
 import cz.dcb.support.db.jpa.entities.Spznote;
+import cz.dcb.support.db.jpa.entities.Spzstate;
 import cz.dcb.support.db.managers.exceptions.IllegalOrphanException;
 import cz.dcb.support.db.managers.exceptions.NonexistentEntityException;
 import cz.dcb.support.db.managers.exceptions.PreexistingEntityException;
 import cz.dcb.support.db.managers.utils.DBUtils;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManagerFactory;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -26,6 +36,8 @@ import static org.junit.Assert.*;
  */
 public class SpznoteManagerTest {
     private final SpzNoteManager manager = new SpzNoteJpaController(DBUtils.getEntityManagerFactory());
+    private static final Logger LOGGER = Logger.getLogger(SpznoteManagerTest.class.getName());
+    private static final int MAX_SPZ_NOTES = 90;
     public SpznoteManagerTest() {
         
     }
@@ -40,10 +52,22 @@ public class SpznoteManagerTest {
     
     @Before
     public void setUp() {
+        clearDB();
+    }
+
+    private void clearDB() {
+        for(Spznote note:manager.findSpznoteEntities()){
+            try {
+                manager.destroy(note.getId());
+            } catch (cz.dcb.support.db.jpa.controllers.exceptions.NonexistentEntityException ex) {
+                LOGGER.log(Level.SEVERE, null, ex);
+            }
+        }
     }
     
     @After
     public void tearDown() {
+        clearDB();
     }
 
     /**
@@ -53,34 +77,80 @@ public class SpznoteManagerTest {
     public void testCreate() throws Exception {
         System.out.println("create");
         Spznote spznote = null;
+        try{
+            manager.create(spznote);
+            fail("Exception should be thrown.");
+        }catch(IllegalArgumentException iae){
+            LOGGER.log(Level.INFO,"Exception ", iae);
+        }catch(Exception ex){
+            fail("Unexpected exception "+ex);
+        }
+
+        Spzstate spzstate = DBUtils.createSpzState();
+        spznote = DBUtils.createSpznote(spzstate);
         manager.create(spznote);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Spznote result = manager.findSpznote(spznote.getId());
+        assertEquals(spznote, result);
     }
 
-    /**
-     * Test of destroy method, of class SpznoteManager.
-     */
-    @Test
-    public void testDestroy() throws Exception {
-        System.out.println("destroy");
-        Integer id = null;
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
+   
     /**
      * Test of edit method, of class SpznoteManager.
      */
     @Test
     public void testEdit() throws Exception {
-        System.out.println("edit");
-        Spznote spznote = null;
-        manager.edit(spznote);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Random rand = new Random();
+        int count = rand.nextInt(MAX_SPZ_NOTES)+10;
+        List<Spznote> notes = createSpzNotes(count);
+        int number = 1;
+        for(Spznote note:notes){
+            note.setNotetext("Note text "+number);
+            number++;
+            manager.edit(note);
+            Spznote result = manager.findSpznote(note.getId());
+            assertEquals(note, result);
+        }
     }
 
+    @Test
+    public void testEditInvalidID() throws Exception{
+        int minId = -1;
+        Spzstate state = DBUtils.createSpzState();
+        Spznote note = DBUtils.createSpznote(state);
+        note.setId(minId);
+        manager.edit(note);
+        Spznote result = manager.findSpznote(minId);
+        assertNull(result);
+        int maxId = Collections.max(manager.findSpznoteEntities(), new Comparator<Spznote>() {
+
+            @Override
+            public int compare(Spznote o1, Spznote o2) {
+                return o1.getId()-o2.getId();
+            }
+        }).getId();
+        note.setId(maxId+1);
+        manager.edit(note);
+        result = manager.findSpznote(maxId+1);
+        LOGGER.log(Level.INFO,"result = ",result);
+        assertEquals(note, result);
+        
+    }
+    @Test
+    public void testSpznoteEditInvalidNotetext(){
+        Spzstate state = DBUtils.createSpzState();
+        Spznote note = DBUtils.createSpznote(state);
+        String text = createRandomText(8000);
+        note.setNotetext(text);
+        manager.create(note);
+        Spznote result = manager.findSpznote(note.getId());
+        assertEquals(note.getNotetext(), result.getNotetext());
+        note.setNotetext(createRandomText(8001));
+        try {
+            manager.edit(note);
+        } catch (Exception ex) {
+            Logger.getLogger(SpznoteManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     /**
      * Test of findSpznote method, of class SpznoteManager.
      */
@@ -163,5 +233,36 @@ public class SpznoteManagerTest {
             return 0;
         }
     }*/
+
+    private List<Spznote> createSpzNotes(int count) {
+        List<Spznote> notes = new ArrayList<>();
+        EntityManagerFactory emf = DBUtils.getEntityManagerFactory();
+        SpzStateManager stateManager = new SpzStateJpaController(emf);
+        
+        for(int i=0;i<count;i++){
+            Spzstate state = DBUtils.createSpzState();
+            stateManager.create(state);
+            Spznote note = DBUtils.createSpznote(state);
+            manager.create(note);
+            notes.add(note);
+        }
+        return notes;
+    }
+
+    private String createRandomText(int len) {
+        Random rand = new Random();
+        StringBuilder builder = new StringBuilder();
+        for(int i=0;i<len;i++){
+            char c ;
+            if(rand.nextBoolean()){
+                c = (char)(rand.nextBoolean()?rand.nextInt('Z'-'A')+'A':rand.nextInt('z'-'a')+'a');
+            }else{
+                c=' ';
+            }
+            builder.append(c);
+            
+        }
+        return builder.toString();
+    }
     
 }
