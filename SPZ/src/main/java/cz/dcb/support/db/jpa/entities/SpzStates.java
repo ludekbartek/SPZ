eed0ffd2e0d046c735e728db6dd0bf1b92ccf8cb
ludekbartek <ludek.bartek@seznam.cz>
2015-09-10 15:58:03 +0200

    Enumeration class with SpzStates.

---
+++ SPZ/src/main/java/cz/dcb/support/db/jpa/entities/SpzStates.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.dcb.support.db.jpa.entities;
+
+/**
+ *
+ * @author bar
+ */
+public enum SpzStates {
+    POSTED, NEW, ANALYLIS, REFINE, SPECIFIED, IMPLEMENTATION, DCB_ACCEPTED, 
+    RELEASED, IMPLEMENTATION_REFINE, RE_IMPLEMENTATION, RE_ANALYSIS, 
+    RECLAIMED, CONFIRMED, CANCELED, INVOICED
+    
+}
