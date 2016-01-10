1384691ca36db1d74eb7ef996b2f008c93abfe4b
ludekbartek <ludek.bartek@seznam.cz>
2016-01-10 10:40:27 +0100

    Rozhrani pro ConfigurationSpzJpaController.

---
+++ SPZ/src/main/java/cz/dcb/support/db/jpa/controllers/ConfigurationSpzManager.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.dcb.support.db.jpa.controllers;
+
+import cz.dcb.support.db.jpa.controllers.exceptions.NonexistentEntityException;
+import cz.dcb.support.db.jpa.controllers.exceptions.PreexistingEntityException;
+import cz.dcb.support.db.jpa.entities.Configurationspz;
+import java.util.List;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author bar
+ */
+public interface ConfigurationSpzManager {
+
+    void create(Configurationspz configurationspz) throws PreexistingEntityException, Exception;
+
+    void destroy(Integer id) throws NonexistentEntityException;
+
+    void edit(Configurationspz configurationspz) throws NonexistentEntityException, Exception;
+
+    Configurationspz findConfigurationspz(Integer id);
+
+    List<Configurationspz> findConfigurationspzEntities();
+
+    List<Configurationspz> findConfigurationspzEntities(int maxResults, int firstResult);
+
+    int getConfigurationspzCount();
+
+    EntityManager getEntityManager();
+
+    public Integer getSpzConfiguration(int spzId);
+    
+}
