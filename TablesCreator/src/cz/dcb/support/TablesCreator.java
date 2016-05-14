18b62ac324195d9658182b23ce8349f455d5cff1
ludekbartek <ludek.bartek@seznam.cz>
2016-05-14 15:48:02 +0200

    Tool for creating db structure should be ok.

--- TablesCreator/src/cz/dcb/support/TablesCreator.java
+++ TablesCreator/src/cz/dcb/support/TablesCreator.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLSyntaxErrorException;
 import java.sql.Statement;
@@ -30,7 +32,7 @@ public class TablesCreator {
      */
     public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
         //Class.forName("org.apache.derby.jdbc.ClientDriver");
-        try(Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/support;create=true","suser","suser")){
+        try(Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/support_test;create=true","suser","suser")){
             BufferedReader reader = new BufferedReader(new InputStreamReader(TablesCreator.class.getResourceAsStream("/cz/dcb/support/support-derby.sql")));
             String line;
             Statement stat = con.createStatement();
@@ -38,11 +40,64 @@ public class TablesCreator {
                 System.out.println("Executing: "+line);
                 try{
                     stat.execute(line);
-                }catch(SQLSyntaxErrorException ex){
-                    Logger.getAnonymousLogger().log(Level.SEVERE, "SQL Syntax error {0}", line);
+                }catch(SQLException ex){
+                    Logger.getAnonymousLogger().log(Level.SEVERE, String.format("SQL exception %s on %s.",ex,line));
                 }
             }
+            int projectId = insertSampleProject(con,"sample project");
+            int configId = insertSampleConfiguration(con,"sample configuration");
+            int managerId = insertUser(con,"manager","Pepa Manager");
+            int developerId = insertUser(con,"analyst","Tonda Vyvojar");
+            int clientId = insertUser(con,"user","Franta Uzivatel");
+            int adminId = insertUser(con,"admin","Pavel Korinek",Roles.ADMIN);
+            setRole(con,configId,managerId,Roles.PROJECT_MANAGER);
+            setRole(con,configId,developerId,Roles.ANALYST);
+            setRole(con,configId,clientId,Roles.CLIENT);
         }
+    }
+
+    private static int insertSampleProject(Connection con, String projectName) throws SQLException {
+        PreparedStatement createProject = con.prepareStatement("insert into project(NAME,description) values(?,?)",Statement.RETURN_GENERATED_KEYS);
+        createProject.setString(1, projectName);
+        createProject.setString(2, "Testovaci projekt");
+        createProject.executeUpdate();
+        ResultSet keys = createProject.getGeneratedKeys();
+        
+        keys.next();
+        return keys.getInt(1);
+    }
+
+    private static int insertSampleConfiguration(Connection con, String configurationName) throws SQLException {
+        PreparedStatement createConfig = con.prepareStatement("insert into configuration(code,description) values (?,?)", Statement.RETURN_GENERATED_KEYS);
+        createConfig.setString(1, configurationName);
+        createConfig.setString(2,"Testovaci konfigurace");
+        createConfig.executeUpdate();
+        ResultSet keys = createConfig.getGeneratedKeys();
+        keys.next();
+        return keys.getInt(1);
+    }
+
+    private static int insertUser(Connection con, String login, String name, Roles role) throws SQLException {
+        PreparedStatement createUser = con.prepareStatement("insert into USER_(LOGIN,NAME,CLASS_TYPE) values(?,?,?)", Statement.RETURN_GENERATED_KEYS);
+        createUser.setString(1, login);
+        createUser.setString(2,name);
+        createUser.setShort(3, (short) role.ordinal());
+        createUser.executeUpdate();
+        ResultSet keys = createUser.getGeneratedKeys();
+        keys.next();
+        return keys.getInt(1);
+    }
+
+    private static int insertUser(Connection con,String login, String name) throws SQLException {
+        return insertUser(con, login, name, Roles.CLIENT);
+    }
+
+    private static void setRole(Connection con,int configId, int userId, Roles role) throws SQLException {
+        PreparedStatement setRole=con.prepareStatement("insert into USERACCESS(ROLE_,USERID,CONFIGURATIONID) values(?,?,?)");
+        setRole.setShort(1, (short)role.ordinal());
+        setRole.setInt(2, userId);
+        setRole.setInt(3, configId);
+        setRole.executeUpdate();
     }
     
 }
