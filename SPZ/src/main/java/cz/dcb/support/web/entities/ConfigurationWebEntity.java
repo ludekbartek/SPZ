e60d0092cf27b2414ab23a3de5c83510874a4049
ludekbartek <ludek.bartek@seznam.cz>
2016-05-27 08:59:08 +0200

    Corrected class visibility to public

--- SPZ/src/main/java/cz/dcb/support/web/entities/ConfigurationWebEntity.java
+++ SPZ/src/main/java/cz/dcb/support/web/entities/ConfigurationWebEntity.java
@@ -11,7 +11,7 @@ import java.util.Objects;
  *
  * @author bar
  */
-class ConfigurationWebEntity {
+public class ConfigurationWebEntity {
     private Integer id;
     private String name;
     private String description;
