b1065035dd769304d80af0e5e5aaacd00421c8e5
ludekbartek <ludek.bartek@seznam.cz>
2016-06-18 19:36:32 +0200

    Vypoctovy typ pro praci s filterovanim SPZ.

---
+++ SPZ/src/main/java/cz/dcb/support/web/Filter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.dcb.support.web;
+
+/**
+ *
+ * @author bar
+ */
+public enum Filter {
+    ALL, OPENED, WAITING_CLIENT, WAITING_IMPL, INSTALLED, ACCEPTED, SOLVED, CANCELED
+}
